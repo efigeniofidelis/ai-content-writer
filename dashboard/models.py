9d36276d57cef5a121bbf2520472c87d1b58849f
“joshualucas0507” <joshualucas0507@gmail.com>
2022-11-16 23:29:56 +0500

    model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.utils import timezone
+from uuid import uuid4
+from django.conf import settings
+from django.urls import reverse
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    title = models.CharField(null=True,blank=True,max_length=200)
+    
+    
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return '{}{}'.format(self.title, self.uniqueId)
+
+
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{}{}'.format(self.title, self.uniqueId))
+
+
+        self.slug = slugify('{}{}'.format(self.title, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+        super(Profile, self).save(*args, **kwargs)
